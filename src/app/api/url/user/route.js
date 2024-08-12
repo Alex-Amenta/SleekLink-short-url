486938d6324f9757938088773219c391dfcd1972
Alex-Amenta <thekinggcat@gmail.com>
2024-08-12 17:38:13 -0300

    fix: update packages

--- src/app/api/url/user/route.js
+++ src/app/api/url/user/route.js
@@ -1,9 +1,9 @@
 import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
-import { authOptions } from "@auth/[...nextauth]/route.js";
+import { authOptions } from "../../auth/[...nextauth]/route";
 
-export async function GET(request) {
+export async function GET() {
     try {
         const session = await getServerSession(authOptions);
         const email = session?.user.email;
